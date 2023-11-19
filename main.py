38a8e6f3d779f56cf191b7c386349a6245228e05
Aashish Harishchandre <aashish0415@gmail.com>
2023-11-18 20:40:43 -0500

    Update main.py
    Removed "Make a Donation/Transfer Money" and "Live Donation" and replaced it with Account A/B/C indicating the something you are donating to. Also added current_page which keeps track of what page the user is on (setting up for the backend JavaScript).

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import matplotlib.pyplot as plt
 import math
 
 
-def send_money(sender, receiver, amount):
-    return f"{sender} sent {amount} to {receiver}."
+def send_money(sender, receiver, amount, currency, description):
+    return f"{sender} sent {amount} {currency} to {receiver} for {description}."
 
 if 'loggedin' not in st.session_state:
     st.session_state['loggedin'] = False
 
+if 'username' not in st.session_state:
+    st.session_state['username'] = ''
+
+if 'transactions' not in st.session_state:
+    st.session_state['transactions'] = []
+
 if 'donations' not in st.session_state:
     st.session_state['donations'] = []
+
+if 'current_page' not in st.session_state:
+    st.session_state['current_page'] = ''
 
 if not st.session_state['loggedin']:
     st.title("Welcome to our Charity Blockchain!")
@@ -24,6 +33,7 @@ if not st.session_state['loggedin']:
         This application serves a dual purpose: it supports open source developers and aids those in need, such as charities feeding the homeless, and more. Think of this as a blend of charity and support, leveraging the power of decentralized technology for good.
         </div>
         """, unsafe_allow_html=True)
+    
     with st.form(key='login_form'):
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
@@ -40,28 +50,51 @@ else:
     if st.session_state['loggedin']:
         if st.sidebar.button('Logout'):  # Add a logout button in the sidebar
             st.session_state['loggedin'] = False
-            st.info('Click again to logout.')
-        st.title(f"Welcome back, {st.session_state['username']}!")
+            st.info('Logged out successfully.')
+            
+    st.sidebar.image("https://th.bing.com/th/id/OIG.ityO4.xUVd6WFLRfzImF?pid=ImgGn", use_column_width=True)
 
-    page = st.sidebar.selectbox("Choose a page", ["Make a Donation/Transfer Money", "Live Donations"])
+    st.title(f"Welcome back, {st.session_state['username']}!")
 
-    if page == "Make a Donation/Transfer Money":
-        st.subheader("Transaction History")
-        sender = st.session_state['username']
-        receiver = st.sidebar.text_input("Receiver")
-        amount = st.sidebar.number_input("Amount", min_value=1, step=5)  # Set step to 5
-        currency = st.sidebar.selectbox("Choose a currency", ["USD", "EUR", "BTC", "ETH"])
-        description = st.sidebar.text_input("Description")
+    page = st.sidebar.selectbox("Choose a Donation", ["Account A", "Account B", "Account C"])
+    st.session_state['current_page'] = page
 
-        if st.sidebar.button("Send"):
-            result = send_money(sender, receiver, amount, currency, description)
-            st.session_state['transactions'].append({"Sender": sender, "Receiver": receiver, "Amount": amount, "Currency": currency, "Description": description})
-            st.write(result)
-        else:
-            st.write("Enter the transaction details.")
+    if page == "Account A":
+        st.subheader("Account A")
 
-    elif page == "Live Donations":
-        st.subheader("Live Donations")
+        total_raised = sum([donation['Amount'] for donation in st.session_state['donations'] if 'Amount' in donation])
+        goal_amount = 10000
+
+        with st.form(key='donation_form'):
+            donation_amount = st.number_input('Enter donation amount', min_value=1)
+            submit_button = st.form_submit_button(label='Donate')
+
+            if submit_button:
+                st.session_state['donations'].append({"Donor": st.session_state['username'], "Amount": donation_amount})
+                st.success(f"You donated ${donation_amount}! Thank you for your generosity.")
+                total_raised += donation_amount
+                progress = min(total_raised / goal_amount, 1)
+                st.progress(progress)
+
+    elif page == "Account B":
+        st.subheader("Account B")
+
+        total_raised = sum([donation['Amount'] for donation in st.session_state['donations'] if 'Amount' in donation])
+        goal_amount = 10000
+
+        with st.form(key='donation_form'):
+            donation_amount = st.number_input('Enter donation amount', min_value=1)
+            submit_button = st.form_submit_button(label='Donate')
+
+            if submit_button:
+                st.session_state['donations'].append({"Donor": st.session_state['username'], "Amount": donation_amount})
+                st.success(f"You donated ${donation_amount}! Thank you for your generosity.")
+                total_raised += donation_amount
+                progress = min(total_raised / goal_amount, 1)
+                st.progress(progress)
+    
+    elif page == "Account C":
+        st.subheader("Account C")
 
         total_raised = sum([donation['Amount'] for donation in st.session_state['donations'] if 'Amount' in donation])
         goal_amount = 10000
